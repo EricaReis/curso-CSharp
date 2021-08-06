a42bb9504d739e57bb87914613c59efdcb7ee2d5
EricaReis <erica@cloudmed.io>
2021-08-06 17:31:34 -0300

    metodos com retorno

---
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+
+
+
+    class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
